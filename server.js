a2b1bac50c9e9e81e19c3fdeaa6c02440cf6d0ac
alicia chen <aliciachen10@gmail.com>
2021-10-19 16:19:31 -0400

    clean code, get ready to modularize

--- server.js
+++ server.js
@@ -10,16 +10,13 @@ const uuid = require('./helpers/uuid');
 // const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
-
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-// console.log(db[5])
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -62,77 +59,7 @@ const readAndAppend = (content, file) => {
   });
 };
 
-// GET Route for retrieving all the tips
-app.get('/api/tips', (req, res) => {
-  console.info(`${req.method} request received for tips`);
-  readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
-});
-
-// POST Route for a new UX/UI tip
-app.post('/api/tips', (req, res) => {
-  console.info(`${req.method} request received to add a tip`);
-
-  const { username, topic, tip } = req.body;
-
-  if (req.body) {
-    const newTip = {
-      username,
-      tip,
-      topic,
-      tip_id: uuid(),
-    };
-
-    readAndAppend(newTip, './db/tips.json');
-    res.json(`Tip added successfully 🚀`);
-  } else {
-    res.error('Error in adding tip');
-  }
-});
-
-// GET Route for retrieving all the feedback
-app.get('/api/feedback', (req, res) => {
-  console.info(`${req.method} request received for feedback`);
-
-  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
-});
-
-// POST Route for submitting feedback
-app.post('/api/feedback', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to submit feedback`);
-
-  // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
-
-  // If all the required properties are present
-  if (email && feedbackType && feedback) {
-    // Variable for the object we will save
-    const newFeedback = {
-      email,
-      feedbackType,
-      feedback,
-      feedback_id: uuid(),
-    };
-
-    readAndAppend(newFeedback, './db/feedback.json');
-
-    const response = {
-      status: 'success',
-      body: newFeedback,
-    };
-
-    res.json(response);
-  } else {
-    res.json('Error in posting feedback');
-  }
-});
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
-
-//TIPS ROUTES
-// POST Route for a new UX/UI tip
+// POST Route for a eew note
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a tip`);
   console.log(req.body)
@@ -152,26 +79,22 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-//tip route get tip. 
-// GET Route for retrieving all the tips
+// GET Route for retrieving all the notes
 app.get('/api/notes', (req, res) => {
   console.info(`${req.method} request received for tips`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-//tip route get tip. 
-// GET Route for retrieving all the tips
+// GET Route for retrieving a note by id
 app.get('/api/notes/:id', (req, res) => {
   console.info(`${req.method} request received for tips`);
   res.json(notes[id])
 });
 
-//delete route for deleting a note
+//delete route for deleting a note by id
 //go into the db.json and get rid of the note so that the new list of notes can render to the frontend
 app.delete('/api/notes/:id', (req, res) => {
   console.log(req.params)
-  // console.info(`${req.method} request received for tips`);
-  // console.log(`/api/notes/${id}`)
 
   db.find((element, index) => {
     console.log(element)
@@ -183,9 +106,9 @@ app.delete('/api/notes/:id', (req, res) => {
     }
 
   })
-
-  const myid = db.find(element =>  req.params.id == element.id)
-  // console.log(element.id)
   res.json(true)
-  //readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
